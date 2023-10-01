8af8e66376b817b26b3f4e042d7004c20d53c7a3
ozen-hub <info.ozenhub@gmail.com>
2023-10-01 21:47:58 +0530

    10

--- customer-update.php
+++ customer-update.php
@@ -1,21 +1,29 @@
-
 <?php
 include 'header.php';
 include 'mysql_connect.php';
 
-$requestNic=$_GET['customerNic'];
+$requestNic = $_GET['customerNic'];
 
-if (isset($_POST['submit'])){
-    $name=$_POST['name'];
-    $address=$_POST['address'];
-    $salary=$_POST['salary'];
+$query = "SELECT * FROM customer WHERE nic='$requestNic'";
+$selectedCustomer = mysqli_query($con, $query);
+$raw = mysqli_fetch_assoc($selectedCustomer);
+$selectedNic = $raw['nic'];
+$selectedName = $raw['name'];
+$selectedAddress = $raw['address'];
+$selectedSalary = $raw['salary'];
 
-    $sql="UPDATE customer SET name='$name', address='$address',salary=$salary WHERE nic=$requestNic";
+if (isset($_POST['submit'])) {
+    $name = $_POST['name'];
+    $address = $_POST['address'];
+    $salary = $_POST['salary'];
 
-    $result=mysqli_query($con,$sql);
-    if ($result){
+    $sql = "UPDATE customer SET name='$name', address='$address', salary=$salary WHERE nic='$requestNic'";
+
+    $result = mysqli_query($con, $sql);
+    if ($result) {
         header("location:customer-list.php");
     }
+
 
 }
 
@@ -30,7 +38,8 @@ if (isset($_POST['submit'])){
           content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
 
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet"
+          integrity="sha384-T3c6CoIi6uLrA9TneNEoa7RxnatzjcDSCmG1MXxSR1GAsXEV/Dwwykc2MPK8M2HN" crossorigin="anonymous">
 
     <title>Customers</title>
 </head>
@@ -43,19 +52,25 @@ if (isset($_POST['submit'])){
         <div class="row">
             <div class="col-3">
                 <div class="form-group">
-                    <label for="nic">Name</label>
+                    <label for="nic">Nic</label>
+                    <input type="text" name="nic" id="nic" class="form-control" required>
+                </div>
+            </div>
+            <div class="col-3">
+                <div class="form-group">
+                    <label for="name">Name</label>
                     <input type="text" name="name" id="name" class="form-control" required>
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <label for="nic">Address</label>
+                    <label for="address">Address</label>
                     <input type="text" name="address" id="address" class="form-control" required>
                 </div>
             </div>
             <div class="col-3">
                 <div class="form-group">
-                    <label for="nic">Salary</label>
+                    <label for="salary">Salary</label>
                     <input type="number" name="salary" id="salary" class="form-control" required>
                 </div>
             </div>
