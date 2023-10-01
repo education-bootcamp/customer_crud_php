189bfb10a3b3814dc6eb791d8effbe36204beb47
ozen-hub <info.ozenhub@gmail.com>
2023-10-01 20:52:21 +0530

    5

--- customer-list.php
+++ customer-list.php
@@ -14,5 +14,51 @@ include 'mysql_connect.php';
 </head>
 <body>
 
+
+<div class="container">
+    <br>
+    <h4>Customer Form</h4>
+    <hr>
+
+    <table class="table table-hover">
+        <thead>
+        <tr>
+            <th>Nic</th>
+            <th>Name</th>
+            <th>Address</th>
+            <th>Salary</th>
+        </tr>
+        </thead>
+        <tbody>
+        <?php
+
+        $sql="SELECT * FROM customer";
+        $result=mysqli_query($con,$sql);
+        if($result){
+            while ($raw=mysqli_fetch_assoc($result)){
+                $nic=$raw['nic'];
+                $name=$raw['name'];
+                $address=$raw['address'];
+                $salary=$raw['salary'];
+
+                echo '
+                <tr>
+                    <td>'.$nic.'</td>
+                    <td>'.$name.'</td>
+                    <td>'.$address.'</td>
+                    <td>'.$salary.'</td>
+                </tr>
+                ';
+
+            }
+        }
+
+        ?>
+
+        </tbody>
+    </table>
+
+</div>
+
 </body>
 </html>
